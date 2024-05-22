bddd049876b53a652d84ee356b42cb7bcfebbdc0
Guilherme Robson Hilario <guilhermehilariosilva@gmail.com>
2024-05-22 09:45:34 -0300

    feat: create register view

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import "./styles/global.css";
 import "./styles/reset.css";
 import "./styles/app.css";
-import { Login } from "./presentation/pages";
+import { Login, Register } from "./presentation/pages";
 
 function App() {
   return (
     <div className="content">
-      <Login />
+      {/* <Login />  */}
+      <Register />
     </div>
   );
 }
---
+++ src/presentation/pages/register/register-view.tsx
@@ -0,0 +1,16 @@
+import { Button, ButtonGroup, Container, Input } from "../../components";
+
+export function RegisterView() {
+  return (
+    <Container>
+      <Input name="name" placeholder="Name" />
+      <Input name="email" placeholder="E-mail" />
+      <Input name="cpf-cnpj" placeholder="CPF/CNPJ" />
+      <Input name="phone" placeholder="Phone" />
+      <ButtonGroup>
+        <Button>Cancel</Button>
+        <Button>Register</Button>
+      </ButtonGroup>
+    </Container>
+  );
+}
