0ae683ee7d63f66443098c6067c7b508ee010a47
Guilherme Robson Hilario <guilhermehilariosilva@gmail.com>
2024-05-23 10:48:34 -0300

    refactor: menu changes

--- src/presentation/components/lateral-menu/lateral-menu.tsx
+++ src/presentation/components/lateral-menu/lateral-menu.tsx
@@ -3,11 +3,18 @@ import { LateralMenuProps } from "./type";
 
 export function LateralMenu({ itemsMenu }: LateralMenuProps) {
   const listItems = itemsMenu.map((element) => (
-    <li key={element.name}>{element.name}</li>
+    <li key={element.name}>
+      <a href={element.component}>{element.name}</a>
+    </li>
   ));
   return (
     <div className="container-menu">
+      <div className="perfil">
+        <p>ICON</p>
+        <a href="/perfil">Fulano Ciclano de Tal</a>
+      </div>
       <ul>{listItems}</ul>
+      <a href="/logout">Logout</a>
     </div>
   );
 }
