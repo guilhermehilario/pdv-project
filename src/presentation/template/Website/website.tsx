a9c01fdf831199d9c5fab6a39f1b86089341a60f
Guilherme Robson Hilario <guilhermehilariosilva@gmail.com>
2024-06-04 15:19:21 -0300

    refactor: website

--- src/presentation/template/Website/website.tsx
+++ src/presentation/template/Website/website.tsx
@@ -2,7 +2,7 @@ import "./website.css";
 
 import { Outlet } from "react-router-dom";
 
-import { LateralMenu } from "../../components";
+import { Container, LateralMenu } from "../../components";
 import { useDashboardViewModel } from "../../model";
 
 export function Website() {
@@ -10,7 +10,9 @@ export function Website() {
   return (
     <div className="website">
       <LateralMenu itemsMenu={itemsMenu} />
-      <Outlet />
+      <Container>
+        <Outlet />
+      </Container>
     </div>
   );
 }
